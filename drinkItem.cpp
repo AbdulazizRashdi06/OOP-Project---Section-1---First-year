0b0fa258963062c590849cc2ce3fe2e451b38e44
Yaamen al Zadjali <whz2.yaa@gmail.com>
2026-04-28 02:26:58 +0400

    added drink item class

---
+++ drinkItem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by whz2y on 4/28/2026.
+//
+
+#include "drinkItem.h"
---
+++ drinkItem.h
@@ -0,0 +1,13 @@
+//
+// Created by whz2y on 4/28/2026.
+//
+
+#ifndef OOP_PROJECT_YEAR1_DRINKITEM_H
+#define OOP_PROJECT_YEAR1_DRINKITEM_H
+
+
+class drinkItem {
+};
+
+
+#endif //OOP_PROJECT_YEAR1_DRINKITEM_H
