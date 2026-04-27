e0bfcb1f5474cd4b42b50a20a0304902f8ea3f65
Yaamen al Zadjali <whz2.yaa@gmail.com>
2026-04-28 02:17:41 +0400

    added food item class

---
+++ foodItem.h
@@ -0,0 +1,13 @@
+//
+// Created by whz2y on 4/28/2026.
+//
+
+#ifndef OOP_PROJECT_YEAR1_FOODITEM_H
+#define OOP_PROJECT_YEAR1_FOODITEM_H
+
+
+class foodItem {
+};
+
+
+#endif //OOP_PROJECT_YEAR1_FOODITEM_H
