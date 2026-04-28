86ef064cd3ebd20a5f6a73fee895c5a913472fd2
MrAsnssr <asnssrr@gmail.com>
2026-04-28 13:34:28 +0400

    started in tables

---
+++ table.cpp
@@ -0,0 +1,5 @@
+//
+// Created by abdul on 4/28/2026.
+//
+
+#include "table.h"
---
+++ table.h
@@ -0,0 +1,30 @@
+//
+// Created by abdul on 4/28/2026.
+//
+
+#ifndef OOP_PROJECT_YEAR1_TABLE_H
+#define OOP_PROJECT_YEAR1_TABLE_H
+
+
+class table {
+    int tableNumber;
+    int capacity;
+    bool isOccupied;
+    order currentOrder;
+    public:
+
+    table(int c);
+    int getTableNumber();
+    int getCapacity();
+    bool isEmpty();
+    order getCurrentOrder();
+    void setCapacity(int );
+    void setOccupied(bool );
+
+
+
+
+};
+
+
+#endif //OOP_PROJECT_YEAR1_TABLE_H
