c50966ffe4421589dbb96588a5cc518f16273a63
MrAsnssr <asnssrr@gmail.com>
2026-05-08 15:00:14 +0400

    started in pages functions

---
+++ pages.cpp
@@ -0,0 +1,11 @@
+//
+// Created by abdul on 5/8/2026.
+//
+
+#include "pages.h"
+
+void mainPage(menu &m, order &o, table &t, waiter &w) {
+
+
+
+}
---
+++ pages.h
@@ -0,0 +1,17 @@
+//
+// Created by abdul on 5/8/2026.
+//
+
+#ifndef OOP_PROJECT_YEAR1_PAGES_H
+#define OOP_PROJECT_YEAR1_PAGES_H
+
+
+void mainPage(menu &m, order &o, table &t, waiter &w);
+void menuPage();
+void orderPage();
+void tablePage();
+void waiterPage();
+void billPage();
+
+
+#endif //OOP_PROJECT_YEAR1_PAGES_H
