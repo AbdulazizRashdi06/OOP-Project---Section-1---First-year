e3d86c223111813c97bdaa9d45bd2cb6f68f6639
al9ult <24-0042@student.gutech.edu.om>
2026-05-08 13:37:05 +0400

    Update bill.h

--- bill.h
+++ bill.h
@@ -1,7 +1,3 @@
-//
-// Created by abdul on 5/7/2026.
-//
-
 #ifndef OOP_PROJECT_YEAR1_BILL_H
 #define OOP_PROJECT_YEAR1_BILL_H
 #include "order.h"
@@ -10,22 +6,24 @@ class bill {
 private:
     order billOrder;
     float TaxLessAmount;
-    float tax;
-    float discount;
+    float taxPercent;
+    float discountPercent;
+    float taxAmount;
+    float discountAmount;
     float finalAmount;
 
 public:
     bill(order o, float t, float d);
-
-
+    
     float calculateBill();
+    
     void setTax(float t);
     void setDiscount(float d);
-
-
+    
     float getTaxLessAmount();
+    float getFinalAmount();
+    
     void displayBill();
 };
 
-
-#endif //OOP_PROJECT_YEAR1_BILL_H
+#endif
